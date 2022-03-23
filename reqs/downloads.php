97f81df277991c7cb61d21af9ae7d61d42ee6919
Dr3as <dr3as@dr3as.net>
2022-03-23 13:48:46 +0100

    .

---
+++ reqs/downloads.php
@@ -0,0 +1,41 @@
+<?php
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Attempt select query execution
+$sql_downloads = "SELECT * FROM downloads";
+if($result_downloads = mysqli_query($link, $sql_downloads)){
+    if(mysqli_num_rows($result_downloads) > 0){
+        echo "<table>";
+            echo "<tr>";
+                #echo "<th>id</th>";
+                echo "<th>Session</th>";
+                echo "<th>Timestamp</th>";
+                echo "<th>url</th>";
+                echo "<th>outfile</th>";
+                echo "<th>shasum</th>";
+            echo "</tr>";
+        while($row_downloads = mysqli_fetch_array($result_downloads)){
+            echo "<tr>";
+                #echo "<td>" . $row_downloads['id'] . "</td>";
+                echo "<td>" . $row_downloads['session'] . "</td>";
+                echo "<td>" . $row_downloads['timestamp'] . "</td>";
+                echo "<td>" . $row_downloads['url'] . "</td>";
+                echo "<td>" . $row_downloads['outfile'] . "</td>";
+                echo "<td>" . $row_downloads['shasum'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result_downloads);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql_downloads. " . mysqli_error($link);
+}
+// Close connection
+mysqli_close($link);
+?>
