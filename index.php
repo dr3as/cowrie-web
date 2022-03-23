835a64671adcbdd61d0d629357508ec36ba28c77
Dr3as <dr3as@dr3as.net>
2022-03-23 13:56:13 +0100

    .

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <head>
 <title>Cowrie Logs</title>
 <style type="text/css">
+body {background-color: black; color: lightgrey;}
 .tg  {border-collapse:collapse;border-spacing:0;}
 #.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px; overflow:hidden;padding:10px 5px;word-break:normal;}
 #.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px; font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
