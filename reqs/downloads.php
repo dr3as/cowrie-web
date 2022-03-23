add08058765778cc4a6ccc478f01cb9df531595c
Dr3as <dr3as@dr3as.net>
2022-03-23 16:14:03 +0100

    .

--- reqs/downloads.php
+++ reqs/downloads.php
@@ -5,7 +5,7 @@ if($link === false){
 }
 
 // Attempt select query execution
-$sql_downloads = "SELECT DISTINCT url, shasum FROM downloads WHERE shasum IS NOT NULL";
+$sql_downloads = "SELECT url, DISTINCT shasum FROM downloads WHERE shasum IS NOT NULL";
 if($result_downloads = mysqli_query($link, $sql_downloads)){
     if(mysqli_num_rows($result_downloads) > 0){
         echo "<table>";
