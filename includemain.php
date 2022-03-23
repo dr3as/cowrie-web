78778201335e7d4db815ef310d510efeae769258
Dr3as <dr3as@dr3as.net>
2022-03-23 09:15:05 +0100

    .

--- includemain.php
+++ includemain.php
@@ -111,9 +111,9 @@ if($result_auth_password_count = mysqli_query($link, $sql_auth_password_count)){
     echo "ERROR: Could not able to execute $sql_auth_password_count. " . mysqli_error($link);
 }
 
-$sql_auth_password_count = "SELECT username, password, count(*) as password_count FROM auth GROUP BY username, password ORDER BY password_count DESC LIMIT 25";
-if($result_auth_password_count = mysqli_query($link, $sql_auth_password_count)){
-    if(mysqli_num_rows($result_auth_password_count) > 0){
+$sql_auth_username_password_count = "SELECT username, password, count(*) as username_password_count FROM auth GROUP BY username, password ORDER BY username_password_count DESC LIMIT 25";
+if($result_auth_username_password_count = mysqli_query($link, $sql_auth_username_password_count)){
+    if(mysqli_num_rows($result_auth_username_password_count) > 0){
         echo "<h3>Top 25 Combinations</h3>";
         echo "<table>";
             echo "<tr>";
@@ -124,24 +124,24 @@ if($result_auth_password_count = mysqli_query($link, $sql_auth_password_count)){
                 echo "<th>Count</th>";
                 #echo "<th>timestamp</th>";
             echo "</tr>";
-        while($row_auth_password_count = mysqli_fetch_array($result_auth_password_count)){
+        while($row_auth_username_password_count = mysqli_fetch_array($result_auth_username_password_count)){
             echo "<tr>";
                 #echo "<td>" . $row_auth['id'] . "</td>";
                 #echo "<td>" . $row_auth['session'] . "</td>";
-                echo "<td>" . $row_auth_password_count['username'] . "</td>";
-                echo "<td>" . $row_auth_password_count['password'] . "</td>";
-                echo "<td>" . $row_auth_password_count['password_count'] . "</td>";
+                echo "<td>" . $row_auth_username_password_count['username'] . "</td>";
+                echo "<td>" . $row_auth_username_password_count['password'] . "</td>";
+                echo "<td>" . $row_auth_username_password_count['username_password_count'] . "</td>";
                 #echo "<td>" . $row_auth['timestamp'] . "</td>";
             echo "</tr>";
         }
         echo "</table>";
         // Free result set
-        mysqli_free_result($result_auth_password_count);
+        mysqli_free_result($result_auth_username_password_count);
     } else{
         echo "No records matching your query were found.";
     }
 } else{
-    echo "ERROR: Could not able to execute $sql_auth_password_count. " . mysqli_error($link);
+    echo "ERROR: Could not able to execute $sql_auth_username_password_count. " . mysqli_error($link);
 }
 
 // Close connection
