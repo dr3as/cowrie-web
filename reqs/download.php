1f600f32dee3eb26f140df2faea017509ae4cccb
Dr3as <dr3as@dr3as.net>
2022-03-24 20:28:29 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -11,19 +11,9 @@ if($link === false){
 $sql_download_number = "SELECT DISTINCT url, shasum FROM downloads WHERE shasum != \"NULL\"";
 if($result_download_number = mysqli_query($link, $sql_download_number)){
     if(mysqli_num_rows($result_download_number) > 0){
-        echo "<table>";
-            echo "<tr>";
-                echo "<th>url</th>";
-                echo "<th>shasum</th>";
-            echo "</tr>";
-        while($row_download_number = mysqli_fetch_array($result_download_number)){
-            echo "<tr>";
-                echo "<td>" . $row_download_number['url'] . "</td>";
-                echo "<td><a href=\"index.php?url=download&shasum=". $row_download_number['shasum'] ."\">" . $row_download_number['shasum'] . "</a></td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-        // Free result set
+        echo "This file is downloaded ";
+        echo " times";
+         // Free result set
         mysqli_free_result($result_download_number);
     } else{
         echo "No records matching your query were found.";
