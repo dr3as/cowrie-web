883b929975dc1a52b2c80e970ea02e981059b1f4
Dr3as <dr3as@dr3as.net>
2022-03-24 22:06:09 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -37,7 +37,7 @@ if($result_download_name = mysqli_query($link, $sql_download_name)){
     if(mysqli_num_rows($result_download_name) > 0){
         echo "<br>URL and filename<br>";
         while($row_download_name = mysqli_fetch_array($result_download_name)){
-            if($row_download_name['url'] = ""){
+            if(isset($row_download_name['url'])){
             echo "Unknown";
             }
             else{
