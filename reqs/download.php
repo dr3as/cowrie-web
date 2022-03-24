8112a30ac942310296d3284e70328c97ce9c28ad
Dr3as <dr3as@dr3as.net>
2022-03-24 21:35:38 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -27,7 +27,24 @@ if($result_download_number = mysqli_query($link, $sql_download_number)){
 } else{
     echo "ERROR: Could not able to execute $sql_download_number. " . mysqli_error($link);
 }
-echo "<a href=\"https://www.virustotal.com/gui/file/". $shasum ."\">Link to VirusTotal</a>";
+echo "<br><a href=\"https://www.virustotal.com/gui/file/". $shasum ."\">Link to VirusTotal</a>";
+
+$sql_download_name = "SELECT url FROM downloads where shasum = \"$shasum\"";
+if($result_download_name = mysqli_query($link, $sql_download_name)){
+    if(mysqli_num_rows($result_download_name) > 0){
+        echo "URL and Name";
+        while($row_download_name = mysqli_fetch_array($result_download_name)){
+            echo $row_download_name['url'];
+        }
+         // Free result set
+        mysqli_free_result($result_download_name);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql_download_name. " . mysqli_error($link);
+
+
 // Close connection
 mysqli_close($link);
 ?>
