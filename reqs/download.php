4b59548bb51ea927f1b649d8fa91870a017f149a
Dr3as <dr3as@dr3as.net>
2022-03-24 20:53:42 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -16,9 +16,8 @@ if($result_download_number = mysqli_query($link, $sql_download_number)){
     if(mysqli_num_rows($result_download_number) > 0){
         echo "This file is downloaded ";
         while($row_download_number = mysqli_fetch_array($result_download_number)){
-            echo "<td>" . $row_download_number['download_number'] . "</td>";
+            echo $row_download_number['download_number'];
         }
-        echo $sum;
         echo " times";
          // Free result set
         mysqli_free_result($result_download_number);
