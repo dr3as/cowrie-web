d7dc5a8f4e51861e9ad10a8b6937b1a8fb6ebe19
Dr3as <dr3as@dr3as.net>
2022-03-25 08:11:39 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -45,7 +45,8 @@ if($result_download_name = mysqli_query($link, $sql_download_name)){
                 echo dirname($full_url);
                 echo "/  -  ";
                 #$full_url = $row_download_name['url'];
-                echo end(explode("/", $full_url)); 
+                $url_magic = explode("/", $full_url)
+                echo end($url_magic); 
 
             }
             else{
