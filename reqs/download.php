c71aa9c0d485232a6a54a1867fab38ae2ff98732
Dr3as <dr3as@dr3as.net>
2022-03-25 07:50:46 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -32,7 +32,7 @@ echo "<br><a href=\"https://www.virustotal.com/gui/file/". $shasum ."\">Link to 
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$sql_download_name = "SELECT url, shasum FROM downloads WHERE shasum = \"$shasum\"";
+$sql_download_name = "SELECT DISTINCT url, shasum FROM downloads WHERE shasum = \"$shasum\"";
 if($result_download_name = mysqli_query($link, $sql_download_name)){
     if(mysqli_num_rows($result_download_name) > 0){
         echo "<br>URL and filename<br>";
