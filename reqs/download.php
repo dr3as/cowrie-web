6ee58537b18acf91a8832318088740700fd227b9
Dr3as <dr3as@dr3as.net>
2022-03-25 12:38:33 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -71,7 +71,46 @@ if($result_download_name = mysqli_query($link, $sql_download_name)){
     echo "ERROR: Could not able to execute $sql_download_name. " . mysqli_error($link);
 
 }
-echo "</table></td><td style=\"border-style:none;\">test</td></tr></thead></table>";
+echo "</table></td><td style=\"border-style:none;\">";
+if($result_download_name = mysqli_query($link, $sql_download_name)){
+    if(mysqli_num_rows($result_download_name) > 0){
+        
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>URL</th>";
+                echo "<th>Filename</th>";
+            echo "</tr>";
+        while($row_download_name = mysqli_fetch_array($result_download_name)){
+            if(isset($row_download_name['url']) && $row_download_name['url'] != ""){
+                $full_url = $row_download_name['url'];
+                echo "<tr>";
+                echo "<td>";
+                #Get only the url and not the filename;
+                echo dirname($full_url);
+                echo "/ </td>";
+                #Get Filename from full url
+                $url_magic = explode("/", $full_url);
+                echo "<td>";
+                echo end($url_magic); 
+                echo "</td>";
+                echo "</tr>";
+
+            }
+            
+            } 
+            
+
+         // Free result set
+        mysqli_free_result($result_download_name);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql_download_name. " . mysqli_error($link);
+
+}
+
+echo "</table></td></tr></thead></table>";
 // Close connection
 mysqli_close($link);
 ?>
