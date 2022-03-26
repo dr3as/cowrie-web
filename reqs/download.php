5a947f6bedff27bcc90415debf45fd2e67d141ec
Dr3as <dr3as@dr3as.net>
2022-03-26 20:27:34 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -88,8 +88,10 @@ if($result_download_sessions = mysqli_query($link, $sql_download_sessions)){
                 
                 echo "<tr>";
                 echo "<td>";
+                echo "<a href=\"index.php?url=session&session=". $row_download_sessions['session'] ."\">"
                 #Get only the url and not the filename;
-                echo $row_download_sessions['session']; 
+                echo $row_download_sessions['session'];
+                echo "</a>";
                 echo "</td>";
                 echo "</tr>";
 
