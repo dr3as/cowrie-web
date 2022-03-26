cb70ad2c32b68d423e95939ab286bee106f87285
Dr3as <dr3as@dr3as.net>
2022-03-26 22:33:52 +0100

    .

--- reqs/session.php
+++ reqs/session.php
@@ -46,7 +46,7 @@ if($result_session_commands = mysqli_query($link, $sql_session_commands)){
 if($result_session_downloads = mysqli_query($link, $sql_session_downloads)){
     if(mysqli_num_rows($result_session_downloads) > 0){
         while($row_session_downloads = mysqli_fetch_array($result_session_downloads)){
-            echo $row_session_downloads['input'];
+            echo $row_session_downloads['url'];
             echo "<br>";
         }
         // Free result set
