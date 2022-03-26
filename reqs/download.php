e307e23e99f8fab7df1e68f8b097ff6a67b59873
Dr3as <dr3as@dr3as.net>
2022-03-26 20:30:04 +0100

    .

--- reqs/download.php
+++ reqs/download.php
@@ -9,8 +9,6 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 // Attempt select query execution
-#$sql_auth_username_count = "SELECT username, count(*) as username_count FROM auth GROUP BY username ORDER BY username_count DESC LIMIT 25";
-#$row_auth_username_count['username_count']
 $sql_download_number = "SELECT shasum, count(*) as download_number FROM downloads where shasum = \"$shasum\"";
 if($result_download_number = mysqli_query($link, $sql_download_number)){
     if(mysqli_num_rows($result_download_number) > 0){
