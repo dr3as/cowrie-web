2e60a769c69280b495fecbfff0f8a8a95a26434a
Dr3as <dr3as@dr3as.net>
2022-03-26 22:02:31 +0100

    .

--- reqs/session.php
+++ reqs/session.php
@@ -20,6 +20,22 @@ if($result_session_ip = mysqli_query($link, $sql_session_ip)){
                 $row_client_version = mysqli_fetch_row($client);
                 echo $row_client_version['0'];
         }
+        ##
+        $sql_session_commands = "SELECT input FROM input where session = \"$session\"";
+if($result_session_commands = mysqli_query($link, $sql_session_commands)){
+    if(mysqli_num_rows($result_session_commands) > 0){
+        while($row_session_commands = mysqli_fetch_array($result_session_commands)){
+            echo $row_session_commands['input'];
+        }
+        // Free result set
+        mysqli_free_result($result_session_commands);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql_session_commands. " . mysqli_error($link);
+}
+##
         }
             // Free result set
         mysqli_free_result($result_session_ip);
